dc3d55fb7d99edfca68740015ffb38d171f2de09
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2025-02-14 21:14:07 +0900

    DBのURLを修正

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     # データベースのURIを環境変数から取得
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://user:GDOUzrnVRdL4jivgQrLy7chD9t9AfVmW@dpg-cumug7aj1k6c73b442gg-a.singapore-postgres.render.com/pdf_db_957f")
     # SQLAlchemyの変更追跡を無効にする
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
